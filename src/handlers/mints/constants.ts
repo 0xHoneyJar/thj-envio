91c2f1e27474ef8d1831b0c625bc47f513070241
zerker <zerkereth@gmail.com>
2025-09-30 18:44:27 -0700

    Update constants.ts

--- src/handlers/mints/constants.ts
+++ src/handlers/mints/constants.ts
@@ -7,6 +7,7 @@
 export const MINT_COLLECTION_KEYS: Record<string, string> = {
   "0x048327a187b944ddac61c6e202bfccd20d17c008": "mibera_vm",
   "0x80283fbf2b8e50f6ddf9bfc4a90a8336bc90e38f": "mibera_drugs",
+  "0xeca03517c5195f1edd634da6d690d6c72407c40c": "mibera_drugs",
 };
 
 export const CANDIES_MARKET_ADDRESS =
