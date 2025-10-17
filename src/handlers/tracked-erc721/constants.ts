357428ce6cb210a4b4d7e0440d21fb389aaf83ef
zerker <zerkereth@gmail.com>
2025-10-17 13:11:53 -0700

    Track additional Mibera ERC721 collections

--- src/handlers/tracked-erc721/constants.ts
+++ src/handlers/tracked-erc721/constants.ts
@@ -1,3 +1,14 @@
 export const TRACKED_ERC721_COLLECTION_KEYS: Record<string, string> = {
   "0x6666397dfe9a8c469bf65dc744cb1c733416c420": "mibera",
+  "0x4b08a069381efbb9f08c73d6b2e975c9be3c4684": "tarot",
+  "0x86db98cf1b81e833447b12a077ac28c36b75c8e1": "miparcels",
+  "0x8d4972bd5d2df474e71da6676a365fb549853991": "miladies",
+  "0x144b27b1a267ee71989664b3907030da84cc4754": "mireveal_1_1",
+  "0x72db992e18a1bf38111b1936dd723e82d0d96313": "mireveal_2_2",
+  "0x3a00301b713be83ec54b7b4fb0f86397d087e6d3": "mireveal_3_3",
+  "0x419f25c4f9a9c730aacf58b8401b5b3e566fe886": "mireveal_4_20",
+  "0x81a27117bd894942ba6737402fb9e57e942c6058": "mireveal_5_5",
+  "0xaab7b4502251ae393d0590bab3e208e2d58f4813": "mireveal_6_6",
+  "0xc64126ea8dc7626c16daa2a29d375c33fcaa4c7c": "mireveal_7_7",
+  "0x24f4047d372139de8dacbe79e2fc576291ec3ffc": "mireveal_8_8",
 };
